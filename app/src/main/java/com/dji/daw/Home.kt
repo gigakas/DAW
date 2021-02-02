8bca1a6055887c64d6a94d2fa2b2029598e4da62
guill <guillermo@albanss.com>
2021-02-02 17:57:44 -0500

    Formulario Home OK
    - Pantalla panoramica ok
    - Revisados estilos ok

--- app/src/main/java/com/dji/daw/Home.kt
+++ app/src/main/java/com/dji/daw/Home.kt
@@ -5,7 +5,9 @@ import android.os.*
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.util.*
 
 class Home : AppCompatActivity() {
@@ -13,19 +15,22 @@ class Home : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         hideSystemUI()
-        val btn = findViewById<Button>(R.id.buttonVamosAVolar)
+
+        val fab = findViewById<FloatingActionButton>(R.id.BotonVolarAhora)
 
 
-//        btn.setOnClickListener {
-//
-//            Toast.makeText(this@Home, "Pulse Home.", Toast.LENGTH_SHORT).show()
-//            Handler(Looper.getMainLooper()).postDelayed({
-//                val i = Intent(this@Home,Login::class.java)
-//                startActivity(i) //start new activity
-//                finish()
-//            }, 3000)
-//        }
+        fab.setOnClickListener {
+            // We are showing only toast message. However, you can do anything you need.
+            Toast.makeText(this@Home, "A volar", Toast.LENGTH_SHORT).show()
+            Handler(Looper.getMainLooper()).postDelayed({
+                val i = Intent(this@Home, MainActivity::class.java)
+                startActivity(i) //start new activity
+                finish()
+            }, 3000)
+        }
+
     }
+
 
     private fun hideSystemUI() {
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
@@ -35,7 +40,6 @@ class Home : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
-
 }
 
 
