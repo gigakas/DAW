780ae5aed97b0e9dac6ee34746043ab8f42f9a10
guill <guillermo@albanss.com>
2021-02-02 15:02:17 -0500

    Registro formulario OK

---
+++ app/src/main/java/com/dji/daw/RegistroUsuario.kt
@@ -0,0 +1,11 @@
+package com.dji.daw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroUsuario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_usuario)
+    }
+}
