474aa4a6e057e777a8f1fc8970b3f0001a21e93f
guill <guillermo@albanss.com>
2021-02-03 14:47:58 -0500

    Formulario config OK

---
+++ app/src/main/java/com/dji/daw/Configuracion.kt
@@ -0,0 +1,11 @@
+package com.dji.daw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Configuracion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_configuracion)
+    }
+}
